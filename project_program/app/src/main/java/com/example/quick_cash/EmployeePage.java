130cb2bc0bbac2333b44c25a17aeac934686ce47
zhiqiang <zh719990@dal.ca>
2023-02-07 18:40:29 -0400

    Create 5 Activity Page

---
+++ project_program/app/src/main/java/com/example/quick_cash/EmployeePage.java
@@ -0,0 +1,14 @@
+package com.example.quick_cash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_page);
+    }
+}
