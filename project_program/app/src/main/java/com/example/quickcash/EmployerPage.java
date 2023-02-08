159502696a1ba1431dfc49f7789203d2af97a3ea
zhiqiang <zh719990@dal.ca>
2023-02-08 00:24:17 -0400

    Create 5  Activity page and setup for writing test

---
+++ project_program/app/src/main/java/com/example/quickcash/EmployerPage.java
@@ -0,0 +1,13 @@
+package com.example.quickcash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_page);
+    }
+}
