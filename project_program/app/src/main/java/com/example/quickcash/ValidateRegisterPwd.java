70c25706d89b383fd7ead250c626cccb7870dc24
qianrong <qn286799@dal.ca>
2023-02-12 17:44:37 -0400

    merge the validate for pwd and email

--- project_program/app/src/main/java/com/example/quickcash/ValidateRegisterPwd.java
+++ project_program/app/src/main/java/com/example/quickcash/ValidateRegisterPwd.java
@@ -11,3 +11,20 @@ public class ValidateRegisterPwd {
 
     public boolean validateCreditCard (String credit) {return true;}
 }
+
+
+/*
+* Author:Qianrong Yang
+* Description: for validate the email(check whether it follows the rule, has the righ symbol and format)
+* */
+
+//import androidx.appcompat.app.AppCompatActivity;
+// import android.os.Bundle;
+public class ValidateEmail {
+
+    // Validate using regex
+    public boolean validateEmail(String email){
+        return true;
+    }
+
+}
