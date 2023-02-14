32959b431bbad79c46c1453b60ea85d9eae380ac
Zihao Liu <zh447118@dal.ca>
2023-02-14 19:43:24 -0400

    commit for Junit Test checkPasswordCaseTest()

---
+++ project_program/app/src/androidTest/java/com/example/quick_cash/LoginPageTest.java
@@ -0,0 +1,37 @@
+package com.example.quick_cash;
+
+import static org.junit.Assert.*;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginPageTest {
+    @Rule
+    public ActivityScenarioRule<LoginPage> myRule = new ActivityScenarioRule<>(LoginPage.class);
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        System.gc();
+    }
+}
