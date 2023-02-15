d274739b455b8cdf96cbaa9964b77685c9063467
zhiqiang <zh719990@dal.ca>
2023-02-15 17:14:08 -0400

    implement the login function and passed the relevant tests.we can also transfer to the landing page if we use the correct user email and password

--- project_program/app/src/main/java/com/example/quickcash/LoginPage.java
+++ project_program/app/src/main/java/com/example/quickcash/LoginPage.java
@@ -26,10 +26,21 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        mAuth = FirebaseAuth.getInstance();
 
         //implement login button
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(this);
+
+        TextView registerLink = findViewById(R.id.registerLink);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this,RegisterPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
@@ -53,29 +64,64 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
 
 
     /**
-     *
-     * @param message error
+     * This method used to give a hint for user input
+     * @param message hint for user input
      */
     protected void setStatusMessage(String message) {
         TextView statusLabel = findViewById(R.id.statusLabel);
         statusLabel.setText(message.trim());
     }
 
+    /**
+     * This method is used for login
+     * @param email user's email for login
+     * @param password user's password for login
+     */
+    protected void login(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginPage.this,LandingPage.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+
+                            Toast.makeText(LoginPage.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     public void onClick(View v) {
         String email = getEmail();
         String password = getPassword();
-        String errorMessage = password;
 
         if(email.isEmpty()&&password.isEmpty()){
-            errorMessage = getResources().getString(R.string.EMPTY_All).trim();
+            setStatusMessage(getResources().getString(R.string.EMPTY_All).trim());
         } else if(email.isEmpty()){
-            errorMessage = getResources().getString(R.string.EMPTY_EMAIL).trim();
+            setStatusMessage(getResources().getString(R.string.EMPTY_EMAIL).trim());
         } else if (password.isEmpty()) {
-            errorMessage = getResources().getString(R.string.EMPTY_PASSWORD).trim();
+            setStatusMessage(getResources().getString(R.string.EMPTY_PASSWORD).trim());
+        }else{
+            setStatusMessage(getResources().getString(R.string.LOGIN_NOW).trim());
+            login(email,password);
         }
-        setStatusMessage(errorMessage);
 
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent i = new Intent(getApplicationContext(),LandingPage.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
