4894e67af6e406a548ff34ad44d6deffbd17be8a
zhiqiang <zh719990@dal.ca>
2023-02-15 21:56:10 -0400

    implement employer user logout function and passed the relevant test

--- project_program/app/src/main/java/com/example/quickcash/EmployerPage.java
+++ project_program/app/src/main/java/com/example/quickcash/EmployerPage.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 import androidx.annotation.NonNull;
 
@@ -16,5 +18,17 @@ public class EmployerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_page);
+
+        Button logoutBtn = findViewById(R.id.LogoutButton2);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();;
+                Intent intent = new Intent(getApplicationContext(),LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
