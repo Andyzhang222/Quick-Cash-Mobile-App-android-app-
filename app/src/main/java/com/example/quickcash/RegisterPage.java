05ee2e5635e401e9074c503988d24f3d974e93a6
Guanxiang Wang <gn218717@dal.ca>
2023-02-17 18:00:50 -0400

    registration and database connection done

--- app/src/main/java/com/example/quickcash/RegisterPage.java
+++ app/src/main/java/com/example/quickcash/RegisterPage.java
@@ -6,21 +6,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.PatternsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity implements View.OnClickListener{
 
     ValidateRegisterPwd validate = new ValidateRegisterPwd();
+    private EditText editTextCreditCard, editTextEmail, editTextPassword, editTextPasswordRepeat;
+    private Button registerUser;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        Button registerButton = findViewById(R.id.RegisterButton);
-        registerButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser = (Button) findViewById(R.id.RegisterButton);
+        registerUser.setOnClickListener(this);
+
+        editTextCreditCard = (EditText) findViewById(R.id.CardTextBox) ;
+        editTextEmail = (EditText) findViewById(R.id.emailTextBox);
+        editTextPassword = (EditText) findViewById(R.id.passwordTextBox);
+        editTextPasswordRepeat = (EditText) findViewById(R.id.passwordRepeatTextBox);
     }
 
     protected void setStatusMessage(String message) {
@@ -29,23 +48,19 @@ public class RegisterPage extends AppCompatActivity implements View.OnClickListe
     }
 
     protected String getEmail() {
-        EditText email = findViewById(R.id.emailTextBox);
-        return email.getText().toString().trim();
+        return editTextEmail.getText().toString().trim();
     }
 
     protected String getCreditCard() {
-        EditText cc = findViewById(R.id.CardTextBox);
-        return cc.getText().toString().trim();
+        return editTextCreditCard.getText().toString().trim();
     }
 
     protected String getPwd() {
-        EditText password = findViewById(R.id.passwordTextBox);
-        return password.getText().toString().trim();
+        return editTextPassword.getText().toString().trim();
     }
 
     protected String getRepeatPwd() {
-        EditText password = findViewById(R.id.passwordRepeatTextBox);
-        return password.getText().toString().trim();
+        return editTextPasswordRepeat.getText().toString().trim();
     }
 
     protected boolean validatePwdLength(String pwd) {
@@ -122,10 +137,64 @@ public class RegisterPage extends AppCompatActivity implements View.OnClickListe
         else if (!validateRepeatPwd(password,repeatPwd)) {
             pwdErrorMessage = getResources().getString(R.string.PASSWORD_DOES_NOT_MATCH).trim();
             setStatusMessage(pwdErrorMessage);
+        }else {
+            switch (view.getId()){
+                case R.id.RegisterButton:
+                    registerUser();
+                    break;
+            }
         }
 
 
 
     }
 
+    private void registerUser() {
+        String email= editTextEmail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
+        String creditCard = editTextCreditCard.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    User user = new User(email, creditCard, password);
+
+                    FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(RegisterPage.this, "User has been registered successfully", Toast.LENGTH_LONG).show();
+                                    }else{
+                                        Toast.makeText(RegisterPage.this, "Fail to register! Try again!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                }else{
+                    Toast.makeText(RegisterPage.this, "This Email has been registered! Please change the email!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
+class User {
+
+    public String email, creditCard, password;
+
+    public User(){
+
+    }
+
+    public User(String email, String creditCard, String password) {
+        this.email = email;
+        this.creditCard = creditCard;
+        this.password = password;
+    }
+}
+
