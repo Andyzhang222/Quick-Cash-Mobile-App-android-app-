d2cb0d4e1931eacef2f3451ce25c2df04a46181e
Xinxin Yu <xn255368@dal.ca>
2023-02-18 17:26:37 -0400

    comments the LandingPage

--- app/src/main/java/com/example/quickcash/LandingPage.java
+++ app/src/main/java/com/example/quickcash/LandingPage.java
@@ -1,3 +1,8 @@
+/**
+ * This activity is used for choosing which become employee and employer
+ * Editor: Xinxin Yu
+ */
+
 package com.example.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,20 +22,25 @@ public class LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
+        //click the corresponding button
         Button employeeBtn = findViewById(R.id.employeeButton);
         Button employerBtn = findViewById(R.id.employerButton);
 
         employeeBtn.setOnClickListener(new View.OnClickListener() {
+            //when we click the employee button, we jump to the employee page
             @Override
             public void onClick(View v) {
+                //jump to the employee_page
                 Intent switchIntent = new Intent(LandingPage.this,EmployeePage.class);
                 startActivity(switchIntent);
             }
         });
 
         employerBtn.setOnClickListener(new View.OnClickListener() {
+            //when we click the employer button, we jump to the employer page
             @Override
             public void onClick(View v) {
+                //jump to the employer_page
                 Intent switchIntent = new Intent(LandingPage.this, EmployerPage.class);
                 startActivity(switchIntent);
             }
