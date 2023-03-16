ce2400eab8d06b8cec803cfbae0c8b39ac2f257b
Zihao Liu <zh447118@dal.ca>
2023-03-16 19:48:12 -0300

    comments for add Listener for SubmitJobsActivity

--- app/src/main/java/com/example/quickcash/SubmitJobsActivity.java
+++ app/src/main/java/com/example/quickcash/SubmitJobsActivity.java
@@ -1,14 +1,19 @@
 package com.example.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
-public class SubmitJobsActivity extends AppCompatActivity {
+public class SubmitJobsActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_jobs);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
