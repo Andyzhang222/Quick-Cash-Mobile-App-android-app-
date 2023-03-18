a221efff901fa313e7360428d1bd7e5a1979e5f8
zhiqiang <zh719990@dal.ca>
2023-03-18 00:19:47 -0300

    Test: write test for notification sender

---
+++ app/src/main/java/com/example/quickcash/Notification/NotificationSender.java
@@ -0,0 +1,20 @@
+package com.example.quickcash.Notification;
+
+
+import android.content.Context;
+
+public class NotificationSender implements NotificationCreator {
+
+
+
+    public NotificationSender(Context context) {
+    }
+
+
+
+    public void sendNotification(String title, String content) {
+        //buggy function, fix it
+    }
+
+}
+
