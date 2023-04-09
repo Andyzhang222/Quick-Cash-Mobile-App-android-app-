910b633a5167c745d18a990ad025927edb20d708
zhang <hr473038@dal.ca>
2023-04-09 01:21:00 -0300

    finish the test version 2.0

--- app/src/androidTest/java/com/example/quickcash/PostedJobTest.java
+++ app/src/androidTest/java/com/example/quickcash/PostedJobTest.java
@@ -6,14 +6,20 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.example.quickcash.EmployeePageLayoutTest.clickChildViewWithId;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import android.content.Context;
+import android.os.SystemClock;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -29,18 +35,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
 @RunWith(AndroidJUnit4.class)
-public class ViewJobsActivityTest {
-
-
-
+public class PostedJobTest {
 
     @Rule
-    public ActivityScenarioRule<EmployerPage> myRule = new ActivityScenarioRule<>(EmployerPage.class);
-    public IntentsTestRule<EmployerPage> myIntentRule = new IntentsTestRule<>(EmployerPage.class);
-
-
-
-
+    public ActivityScenarioRule<ViewJobsActivity> myRule = new ActivityScenarioRule<>(ViewJobsActivity.class);
+    public IntentsTestRule<ViewJobsActivity> myIntentRule = new IntentsTestRule<>(ViewJobsActivity.class);
 
     @Test
     public void useAppContext() {
@@ -59,19 +58,36 @@ public class ViewJobsActivityTest {
     public static void tearDown() {
         System.gc();
     }
+
+
+
     @Test
     public void checkvisible() {
-        onView(withId(R.id.checkPostedJob)).perform(click());
+        // onView(withId(R.id.Pay_btn)).perform(click());
+        intended(hasComponent(ViewJobsActivity.class.getName()));
+        onView(withId(R.id.p3)).perform(click()).check(matches(withText("POSTED JOBS")));
+
+    }
+
+    @Test
+    public void back2pro() {
+       // onView(withId(R.id.Pay_btn)).perform(click());
         intended(hasComponent(ViewJobsActivity.class.getName()));
         onView(withId(R.id.p3)).perform(click()).check(matches(withText("POSTED JOBS")));
         onView(withId(R.id.back2pro)).check(matches(withText("back to the profile page")));
     }
+
+
     @Test
-    public void checkBack2profile() {
-        onView(withId(R.id.checkPostedJob)).perform(click());
-        //intended(hasComponent(ViewJobsActivity.class.getName()));
-        onView(withId(R.id.back2pro)).perform(click());
+    public void testPayButton(){
+        SystemClock.sleep(2000);
+//        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(1, clickChildViewWithId(R.id.Pay_btn)));
+        SystemClock.sleep(2000);
+//        Espresso.onView(withId(R.id.recyclerview)).check(matches(atPositionOnView(1, withText("Applied"), R.id.applyButton)));
     }
+
+
+
 
 
 
